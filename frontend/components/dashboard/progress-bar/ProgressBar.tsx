30b1d14da812b7f914778cea6160882169f3899e
ohhowheloveus-dev <oh_how_he_love_us@icloud.com>
2026-09-04 07:23:16 +0900

    feat: 국가별 progress bar 컴포넌트 구현

--- frontend/components/dashboard/progress-bar/ProgressBar.tsx
+++ frontend/components/dashboard/progress-bar/ProgressBar.tsx
@@ -1,7 +1,44 @@
-export default function ProgressBar() {
+import React from "react"
+import { Users, ArrowUpRight } from "lucide-react"
+
+const customerData = [
+  {
+    flag: "🇬🇧",
+    country: "United Kingdom",
+    count: "12,628",
+    percent: 80,
+    color: "bg-emerald-500",
+  },
+  {
+    flag: "🇺🇸",
+    country: "United States",
+    count: "10,628",
+    percent: 70,
+    color: "bg-orange-500",
+  },
+  {
+    flag: "🇸🇪",
+    country: "Sweden",
+    count: "8,628",
+    percent: 60,
+    color: "bg-indigo-500",
+  },
+]
+
+export default function CustomersProgressBar() {
   return (
-    <div className="border rounded-xl flex flex-2 items-center justify-center">
-      <p>ProgressBar</p>
+    <div>
+      {customerData.map((customer) => (
+        <div key={customer.country}>
+          {customer.country} - {customer.percent}%
+          <div className="h-2 w-full rounded-full bg-neutral-800">
+            <div
+              className={`h-full rounded-full ${customer.color}`}
+              style={{ width: `${customer.percent}%` }}
+            />
+          </div>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
